17beb83ad7298f9445c8ba141e9f2df26db7a512
gabrielms-castro <gabrielms.castro@gmail.com>
2025-08-31 15:39:06 -0300

    refactor: storing both next and previous url in both commands

--- src/command_map.ts
+++ src/command_map.ts
@@ -1,31 +1,28 @@
+import { stat } from "fs";
 import { State } from "./state.js";
 
 export async function commandMap(state: State) {
-    try {
-        const response = await state.api.fetchLocations(state.nextLocationsURL) // if not null, will pass the URL saved in current State
+    const response = await state.api.fetchLocations(state.nextLocationsURL) // if not null, will pass the URL saved in current State
 
-        // for next calls
-        state.nextLocationsURL = response.next
+    // for next calls
+    state.nextLocationsURL = response.next
+    state.previousLocationsURL = response.previous
 
-        response.results.forEach((results) => console.log(results.name))
-        console.log()
-
-    } catch (err) {
-        console.log(`Error fetching location-areas: ${err}`)
-    }
+    response.results.forEach((results) => console.log(results.name))
+    console.log()
 }
 
 export async function commandMapBack(state: State) {
-    try {
-        const response = await state.api.fetchLocations(state.previousLocationsURL)
-        
-        state.previousLocationsURL = response.previous
-        
-        response.results.forEach((results) => console.log(results.name))
-        console.log()
-
-    } catch (err) {
-        console.log(`Error fetching location-areas: ${err}`)
+    if (!state.previousLocationsURL) {
+        throw new Error("you're on the first page");
     }
+
+    const response = await state.api.fetchLocations(state.previousLocationsURL)
+    
+    state.nextLocationsURL = response.next
+    state.previousLocationsURL = response.previous
+
+    response.results.forEach((results) => console.log(results.name))
+    console.log()
 }
 
